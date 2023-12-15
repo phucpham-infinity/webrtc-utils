503fd1873178a815d9d0cced76e450db3a303360
phuc.pham <109463712+phucpham-infinity@users.noreply.github.com>
2023-12-16 01:28:20 +0700

    change code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { keys } from "lodash";
 
 export const helloLog = (name: string) => {
-  const data = keys({ name: "phuc", age: 29 }).join("-");
-  console.log("helloLog", data, name);
+  console.log("helloLog", "data", name, keys({ name: 1, age: 2 }).join("-"));
+  return name;
+};
+
+export const helloLog2 = (name: string) => {
+  console.log("helloLog", "data", name);
+  return name;
 };
