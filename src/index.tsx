ef4056eccda7922bbe9a40a2d14f6d03742f1648
phuc.pham <109463712+phucpham-infinity@users.noreply.github.com>
2023-12-16 09:59:42 +0700

    Update code

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,4 @@
-import React from "react";
-
-export const helloLog = (name: string) => {
-  console.log("helloLog", "data", name);
-  return name;
-};
-
 export const helloLog2 = (name: string) => {
   console.log("helloLog", "data", name);
   return name;
-};
-
-export const ComA = () => {
-  return <div>This is demo Component</div>;
 };
